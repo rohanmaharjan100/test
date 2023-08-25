c848b0b5628d0551cdb999f58f8bcc24e8a805b7
Rohan Maharjan <rohan.maharjan@harness.io>
2023-08-25 14:25:26 +0545

    trigger test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Lighthouse Test site</h1>
+        <h1>Hello World </h1>
       </header>
     </div>
   );
