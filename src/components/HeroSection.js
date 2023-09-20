ba9ced9ad3a58b44eba1ecdbb33afef91167a8f1
Rohan Maharjan <rohan.maharjan@harness.io>
2023-09-20 11:31:43 +0545

    finalize

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import facebook from "../assets/facebook-svgrepo-com (1).svg";
 
 const HeroSection = () => {
   return (
-    <section className="bg-gray-900 h-screen flex flex-col items-center justify-center ">
+    <section className="bg-gray-900 h-min-screen flex flex-col items-center justify-center ">
       <div className="py-4 px-4 mx-auto max-w-screen-xl text-center lg:py-8 lg:px-12 flex flex-col items-center">
         <img
           alt="name"
@@ -15,8 +15,9 @@ const HeroSection = () => {
           className="h-48 w-48 rounded-full object-cover object-[69%] mb-8"
         />
 
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-          Hi, I am Rohan Maharjan
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  text-white">
+          Hi, I am <br />
+          <p className= " mt-4 md:text-5xl lg:text-6xl"> Rohan Maharjan</p>
         </h1>
         <h3 className="mb-4 text-xl font-bold tracking-tight leading-none text-gray-400 md:text-xl lg:text-xl dark:text-gray-400">
           Full Stack Web developer
@@ -25,7 +26,7 @@ const HeroSection = () => {
           Experienced web developer with a strong track record in frontend and
           backend development, adept at creating seamless online experiences.
         </p>
-        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        <div className="flex flex-col mb-4 lg:mb-8 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
             href="/view-resume"
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-yellow-300 hover:bg-orange-600 focus:ring-4 focus:ring-gray-100"
@@ -70,7 +71,7 @@ const HeroSection = () => {
               rel="noreferrer noopener"
               className=" mx-4 border border-gray-900 rounded-xl hover:border-white hover:scale-105"
             >
-              <img src={gmail} alt="git " className="h-16" />
+              <img src={gmail} alt="git " className="h-16 " />
             </a>
           </div>
         </div>
